51ee30e513e6bdabeb95de3a724004d94b307d5b
AdilJavatogo <Adil2300@hotmail.com>
2025-11-17 10:44:31 +0100

    ratelimiting

--- Gateway/API.Gateway/Program.cs
+++ Gateway/API.Gateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.RateLimiting;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,11 +8,23 @@ internal class Program
         builder.Services.AddReverseProxy()
             .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+        builder.Services.AddRateLimiter(options =>
+        {
+            options.AddFixedWindowLimiter("fixed", limiterOptions =>
+            {
+                limiterOptions.Window = TimeSpan.FromSeconds(10); // hver 10 sekunder, et nyt vindue
+                limiterOptions.PermitLimit = 20; // maks 20 anmodninger per vindue (per klient)
+                limiterOptions.QueueLimit = 0; // ingen kø (429, hvis for mange request)
+            });
+        });
+
         builder.Services.AddHealthChecks();
 
         var app = builder.Build();
         
         app.MapHealthChecks("/health");
+
+        app.UseRateLimiter();
 
         app.MapReverseProxy();
 
